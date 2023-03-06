1194be121cb5c9c2b595e07adb7889d3363fe8f2
micha <michel.plainchamp@gmail.com>
2023-03-06 14:08:25 +0100

    implemented POST student + error message in app properties

--- src/main/java/com/example/restapi/demo/restapi/student/StudentRepository.java
+++ src/main/java/com/example/restapi/demo/restapi/student/StudentRepository.java
@@ -1,11 +1,17 @@
 package com.example.restapi.demo.restapi.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository
         //Long refers to the type of the ID used in the Student model:
         extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s from Student s where s.email=?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
--- src/main/java/com/example/restapi/demo/restapi/student/StudentService.java
+++ src/main/java/com/example/restapi/demo/restapi/student/StudentService.java
@@ -2,10 +2,13 @@ package com.example.restapi.demo.restapi.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -18,5 +21,15 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent( Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email already present");
+        }
+        studentRepository.save(student);
+
     }
 }
