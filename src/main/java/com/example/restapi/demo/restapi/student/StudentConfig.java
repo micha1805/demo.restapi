6ec8461c2ae9fec222ec5569b4514dc96378cb60
micha <michel.plainchamp@gmail.com>
2023-03-06 10:52:02 +0100

    added config, 2 rows in the DB

---
+++ src/main/java/com/example/restapi/demo/restapi/student/StudentConfig.java
@@ -0,0 +1,41 @@
+package com.example.restapi.demo.restapi.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+
+            StudentRepository repository){
+
+                return args -> {
+                    Student alfred = new Student(
+                            "Alfred",
+                            23,
+                            LocalDate.of(2000, Month.SEPTEMBER, 2),
+                            "alfred@gmail.com"
+
+                    );
+
+                    Student john = new Student(
+                            "John",
+                            23,
+                            LocalDate.of(2001, Month.SEPTEMBER, 2),
+                            "john@gmail.com"
+
+                    );
+
+                    repository.saveAll(
+                            List.of(alfred, john)
+                    );
+                };
+            }
+}
